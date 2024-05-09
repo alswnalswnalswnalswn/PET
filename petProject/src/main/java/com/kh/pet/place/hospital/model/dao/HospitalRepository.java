8a5de442dfa35c3b7463e8556ee65d4cd2787e4b
Beenzinoooooooooo <306kjung@naver.com>
2024-05-09 17:52:26 +0900

    지도 실패..
    지도 해보려다가 실패...

--- petProject/src/main/java/com/kh/pet/place/hospital/model/dao/HospitalRepository.java
+++ petProject/src/main/java/com/kh/pet/place/hospital/model/dao/HospitalRepository.java
@@ -1,10 +1,17 @@
 package com.kh.pet.place.hospital.model.dao;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.pet.place.model.vo.Place;
+
 @Repository
-public class HospitalRepository {
-	
-	
+public class HospitalRepository{
+
+	public List<Place> selectHospital(SqlSessionTemplate sqlSession){
+		return sqlSession.selectList("hospitalMapper.selectHospital");
+	}
 	
 }
--- petProject/src/main/java/com/kh/pet/place/hospital/model/service/HospitalServiceImpl.java
+++ petProject/src/main/java/com/kh/pet/place/hospital/model/service/HospitalServiceImpl.java
@@ -1,9 +1,30 @@
 package com.kh.pet.place.hospital.model.service;
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.kh.pet.place.hospital.model.dao.HospitalRepository;
+import com.kh.pet.place.model.vo.Place;
 
 @Service
 public class HospitalServiceImpl implements HospitalService {
+	
+	@Autowired
+	private HospitalRepository hospitalRepository;
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
+	
+	
+	@Override
+	public List<Place> selectHospital() {
+		return hospitalRepository.selectHospital(sqlSession);
+	}
 	
 
 }
