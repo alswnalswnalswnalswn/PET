986c01bb82df62ad02a4854e1d32d9621d7d3307
minju <chlalswn281@naver.com>
2024-05-10 17:31:02 +0900

    er

--- petProject/src/main/java/com/kh/pet/member/model/dao/MemberRepository.java
+++ petProject/src/main/java/com/kh/pet/member/model/dao/MemberRepository.java
@@ -1,8 +1,11 @@
 package com.kh.pet.member.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.pet.common.model.vo.Animal;
 import com.kh.pet.member.model.vo.Member;
 
 @Repository
@@ -16,12 +19,24 @@ public class MemberRepository {
 		return sqlSession.insert("memberMapper.join", member);
 	}
 
-	public int insertAnimals(SqlSessionTemplate sqlSession, String animalCode) {
-		return sqlSession.insert("memberMapper", animalCode);
+	public int insertAnimals(SqlSessionTemplate sqlSession, List<Animal> animal) {
+		return sqlSession.insert("memberMapper", animal);
 	}
 
 	public int idCheck(SqlSessionTemplate sqlSession, String checkId) {
 		return sqlSession.selectOne("memberMapper.idCheck", checkId);
+	}
+
+	public int checkNick(SqlSessionTemplate sqlSession, String nickname) {
+		return sqlSession.selectOne("memberMapper.checkNick", nickname);
+	}
+
+	public int checkPhone(SqlSessionTemplate sqlSession, String phone) {
+		return sqlSession.selectOne("memberMapper.checkPhone", phone);
+	}
+
+	public int checkEmail(SqlSessionTemplate sqlSession, String email) {
+		return sqlSession.selectOne("memberMapper.checkEmail", email);
 	}
 
 }
