72999d4d58c94b6b6b5d0986b908cab0bac8a6ad
KGunh <rjsgml922@naver.com>
2024-05-23 14:29:26 +0900

    mapper 수정끝

--- petProject/src/main/java/com/kh/pet/date/model/service/DateServiceImpl.java
+++ petProject/src/main/java/com/kh/pet/date/model/service/DateServiceImpl.java
@@ -2,63 +2,62 @@ package com.kh.pet.date.model.service;
 
 import java.util.HashMap;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kh.pet.date.model.dao.DateRepository;
+import com.kh.pet.date.model.dao.DateMapper;
 import com.kh.pet.info.model.vo.Comment;
 import com.kh.pet.info.model.vo.Info;
 import com.kh.pet.info.model.vo.Reply;
-import com.kh.pet.place.model.vo.Place;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class DateServiceImpl implements DateService {
 
-	@Autowired
-	private DateRepository dateRepository;
+
+	private final DateMapper dateMapper;
 	
-	@Autowired
-	private SqlSessionTemplate sqlSession;
+
 
 	@Override
 	public int likeCheck(HashMap<String, Integer> map) {
-		return dateRepository.likeCheck(sqlSession,map);
+		return dateMapper.likeCheck(map);
 	}
 	
 	@Override
 	public int updateCount(int placeNo) {
-		return dateRepository.updateCount(sqlSession,placeNo);
+		return dateMapper.updateCount(placeNo);
 	}
 	
 	@Override
 	public Info selectDate(int placeNo) {
-		return dateRepository.selectDate(sqlSession,placeNo);
+		return dateMapper.selectDate(placeNo);
 	}
 
 	@Override
 	public int insertReply(Reply reply) {
-		return dateRepository.insertReply(sqlSession,reply);
+		return dateMapper.insertReply(reply);
 	}
 
 	@Override
 	public int insertComment(Comment comment) {
-		return dateRepository.insertComment(sqlSession,comment);
+		return dateMapper.insertComment(comment);
 	}
 
 	@Override
 	public int updateRepCom(HashMap<String, String> map) {
-		return dateRepository.updateRepCom(sqlSession,map);
+		return dateMapper.updateRepCom(map);
 	}
 
 	@Override
 	public int insertLike(HashMap<String, Integer> map) {
-		return dateRepository.insertLike(sqlSession,map);
+		return dateMapper.insertLike(map);
 	}
 
 	@Override
 	public int deleteLike(HashMap<String, Integer> map) {
-		return dateRepository.deleteLike(sqlSession,map);
+		return dateMapper.deleteLike(map);
 	}
 
 
